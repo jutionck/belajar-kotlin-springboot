a43b4363278979865c9bae0868788a2c2d47dae2
Jution Candra Kirana <jutionck@gmail.com>
2020-11-15 14:04:02 +0700

    SetUp Project

---
+++ src/main/kotlin/com/enigmacamp/kotlin/kotlinresapi/KotlinRestapiPostgresApplication.kt
@@ -0,0 +1,11 @@
+package com.enigmacamp.kotlin.kotlinresapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinRestapiPostgresApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinRestapiPostgresApplication>(*args)
+}
---
+++ src/test/kotlin/com/enigmacamp/kotlin/kotlinresapi/KotlinRestapiPostgresApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.enigmacamp.kotlin.kotlinresapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinRestapiPostgresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
